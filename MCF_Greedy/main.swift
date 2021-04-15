44132acfb0f0b4d563d2c9d8f2f339e0342f4712
Adriano Gaiotto de Oliveira <adriano.gaiotto@gmail.com>
2021-04-15 12:16:36 -0700

    Update Minimum Cost Flow Assignment

--- MCF_Greedy/main.swift
+++ MCF_Greedy/main.swift
@@ -87,42 +87,50 @@ extension CompPipe: Comparable {
 extension CompPipe: Hashable {}
 
 
-//func startMCF(_ input: [String.SubSequence], _ ans:  String) -> Int {
-func startMCF() {
+func startMCF(_ input: [String.SubSequence], _ ans:  String) -> Int {
+//func startMCF() {
     
-    let firstLine = readLine()!.split(separator: " ")
-//    let firstLine = input[0].split(separator: " ")
-    
-    let n = Int(firstLine[0])!
-    let m = Int(firstLine[1])!
-    let d = Int(firstLine[2])!
-    
-    var pipes = [[(v: Int, w: Int, act: Bool)]](repeating: [], count: n + 1)
-    
-    for i in 0..<m {
+//        let firstLine = readLine()!.split(separator: " ")
+        let firstLine = input[0].split(separator: " ")
         
-        let pipe = readLine()!.split(separator: " ")
-//        let pipe = input[1 + i].split(separator: " ")
+        let n = Int(firstLine[0])!
+        let m = Int(firstLine[1])!
+        let d = Int(firstLine[2])!
         
-        let a = Int(pipe[0])!
-        let b = Int(pipe[1])!
-        let c = Int(pipe[2])!
+        var pipes = [[(v: Int, w: Int, act: Bool)]](repeating: [], count: n + 1)
         
-        var active = false
-        if i < n-1 {
-            active = true
+       
+        var pipe1 = [(u: Int, v: Int, w: Int, act: Bool)]()
+        
+        for i in 0..<m {
+            
+//            let pipe = readLine()!.split(separator: " ")
+            let pipe = input[1 + i].split(separator: " ")
+            
+            let a = Int(pipe[0])!
+            let b = Int(pipe[1])!
+            let c = Int(pipe[2])!
+            
+            var active = false
+            if i < n-1 {
+                active = true
+                if c - d <= 0 {
+                    pipe1.append((u: a, v: b, w: c, act: active))
+                }
+            }
+            
+            pipes[a].append((v: b, w: c, act: active))
+            
         }
         
-        pipes[a].append((v: b, w: c, act: active))
+    //    var days = kruskalMST(pipes, n, m, d)
         
-    }
-    
-    
-    print(kruskalMST(pipes, n, m, d))
-//    return kruskalMST(pipes, n, m, d)
+        let (days, _ ) = kruskalMST(pipes, n, m, d)
+//        print(days)
+    return days
 }
 
-public func kruskalMST(_ graph: [[(v: Int, w: Int, act: Bool)]], _ n: Int,_ m: Int,_ d: Int ) -> Int { //(Int, [(Int, Int, Int, Bool)])  {
+public func kruskalMST(_ graph: [[(v: Int, w: Int, act: Bool)]], _ n: Int,_ m: Int,_ d: Int ) -> (Int, [(Int, Int, Int, Bool)])  {
   var allEdges = [(u: Int, v: Int, w: Int, act: Bool)]()
   var mstEdges = [(u: Int, v: Int, w: Int, act: Bool)]()
   
@@ -131,6 +139,7 @@ public func kruskalMST(_ graph: [[(v: Int, w: Int, act: Bool)]], _ n: Int,_ m: I
         allEdges.append((u: u, v: edge.v, w: edge.w, edge.act))
     }
   }
+    
     
   allEdges.sort { $0.w < $1.w || $0.w == $1.w && $0.act == true }
   
@@ -141,68 +150,97 @@ public func kruskalMST(_ graph: [[(v: Int, w: Int, act: Bool)]], _ n: Int,_ m: I
     mstEdges.append(edge)
   }
     
-    let num = mstEdges.map { $0.act == false ? 1 : 0 }.reduce(0, +)
+    var largest = 0
+    var index = 0
+    var count = 0
+    for p in mstEdges {
+        if p.2 > largest {
+            index = count
+            largest = p.2
+        }
+        count += 1
+    }
     
-//    if num - Int(ans)! == 1 {
-//        return num - 1
-//    }
+    var largest2 = 0
+    var index2 = 0
+    var count2 = 0
+    for p in allEdges {
+        if p.act {
+            if p.w > largest2 {
+                index2 = count2
+                largest2 = p.w
+            }
+            count2 += 1
+        }
+    }
     
-    return num
+    
+    
+    
+    var num = mstEdges.map { $0.act == false ? 1 : 0 }.reduce(0, +)
+    
+    if !mstEdges[index].act {
+        if allEdges[index2].w - d <= 0 {
+            num -= 1
+        }
+    }
+    
+    return (num, mstEdges)
 }
 
-startMCF()
+//startMCF()
 
 
 //
-//var countPass = 0
-//var countWrong = 0
-//let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-//for i in 1...50 {
-//
-////    if i != 33 && i != 36 && i != 39 && i != 40 && i != 44 && i != 45 && i != 33 && i != 49 {
-////        continue
-////    }
-//
-//    let fileURLIn = URL(fileURLWithPath: "/Users/adrianogaiotto/Documents/WMAD/ClassFiles/Swift/AlgorithmsDataStructures/AlgorithmsDataStructures/Assignments/MCF/mcf/mcf.\(i).in", relativeTo: directoryURL)
-//
-//    let fileURLOut = URL(fileURLWithPath: "/Users/adrianogaiotto/Documents/WMAD/ClassFiles/Swift/AlgorithmsDataStructures/AlgorithmsDataStructures/Assignments/MCF/mcf/mcf.\(i).out", relativeTo: directoryURL)
-//
-//    var input = [String.SubSequence]()
-//    var result = ""
-//    do {
-//        // Get the saved data
-//        let savedDataIn = try Data(contentsOf: fileURLIn)
-//        let savedDataOut = try Data(contentsOf: fileURLOut)
-//        // Convert the data back into a string
-//        if let savedString = String(data: savedDataIn, encoding: .utf8) {
-//            input = savedString.split(separator: "\n")
-//
-//        }
-//        if let savedString = String(data: savedDataOut, encoding: .utf8) {
-//            result = String(savedString.split(separator: "\n")[0])
-//
-//        }
-//    } catch {
-//        // Catch any errors
-//        print("Unable to read the file")
+var countPass = 0
+var countWrong = 0
+let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+for i in 1...50 {
+
+//    if i != 33 && i != 36 && i != 39 && i != 40 && i != 44 && i != 45 && i != 33 && i != 49 {
+//        continue
 //    }
-//
-//    let days = startMCF(input, String(result))
-//
-//    if Int(result)! == days {
-//        print("test \(i) - pass - \(days)")
-//        countPass += 1
-//    } else {
-//        print("test \(i) - wrong - \(days), correct - \(result)")
-//        countWrong += 1
-//    }
-//
-//
-//
-//}
-//
-//print("Pass Test: \(countPass) - Wrong Test: \(countWrong)")
-//
+
+    let fileURLIn = URL(fileURLWithPath: "/Users/adrianogaiotto/Documents/WMAD/ClassFiles/Swift/AlgorithmsDataStructures/AlgorithmsDataStructures/Assignments/MCF/mcf/mcf.\(i).in", relativeTo: directoryURL)
+
+    let fileURLOut = URL(fileURLWithPath: "/Users/adrianogaiotto/Documents/WMAD/ClassFiles/Swift/AlgorithmsDataStructures/AlgorithmsDataStructures/Assignments/MCF/mcf/mcf.\(i).out", relativeTo: directoryURL)
+
+    var input = [String.SubSequence]()
+    var result = ""
+    do {
+        // Get the saved data
+        let savedDataIn = try Data(contentsOf: fileURLIn)
+        let savedDataOut = try Data(contentsOf: fileURLOut)
+        // Convert the data back into a string
+        if let savedString = String(data: savedDataIn, encoding: .utf8) {
+            input = savedString.split(separator: "\n")
+
+        }
+        if let savedString = String(data: savedDataOut, encoding: .utf8) {
+            result = String(savedString.split(separator: "\n")[0])
+
+        }
+    } catch {
+        // Catch any errors
+        print("Unable to read the file")
+    }
+
+    let days = startMCF(input, String(result))
+
+    if Int(result)! == days {
+        print("test \(i) - pass - \(days)")
+        countPass += 1
+    } else {
+        print("test \(i) - wrong - \(days), correct - \(result)")
+        countWrong += 1
+    }
+
+
+
+}
+
+print("Pass Test: \(countPass) - Wrong Test: \(countWrong)")
+
 
 
 
